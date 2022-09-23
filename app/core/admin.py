d13b42c917ac75e1a5c23bcaffe8cabed779b947
Peter Gregus <petko.gregus@gmail.com>
2022-09-23 17:30:16 +0200

    Add User API and authentication

---
+++ app/core/admin.py
@@ -0,0 +1,50 @@
+"""
+Django admin cust
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name', 'is_therapist']
+    list_filter = ('is_active', 'is_superuser', 'is_therapist')
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'name')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'is_therapist',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'is_therapist',
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
