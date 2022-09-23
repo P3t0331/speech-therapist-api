1f46c765bf35774311903de876b334b10cf008e4
Peter Gregus <petko.gregus@gmail.com>
2022-09-23 17:30:42 +0200

    Add User API and authentication

---
+++ app/user/serializers.py
@@ -0,0 +1,76 @@
+"""
+Serializers for the user API view
+"""
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+    )
+from django.utils.translation import gettext as _
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user object"""
+    confirm_password = serializers.CharField(
+        allow_blank=False,
+        write_only=True,
+        min_length=8,
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'name', 'password', 'confirm_password']
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'min_length': 8,
+                'style': {'input_type': 'password'}
+                }
+            }
+
+    def validate(self, data):
+        """
+        Checks to be sure that the received password and confirm_password
+        fields are exactly the same
+        """
+        if data['password'] != data.pop('confirm_password'):
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    def create(self, validated_data):
+        """Create and return a user with encrypted password"""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token"""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate and auth the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
